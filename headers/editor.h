4dbf9198977125f4100000a79a4b084d4c834993
Pedro Dias Vicente <pedrolvicente@gmail.com>
2023-08-18 14:35:07 +0100

    c++ parser headers to be implemented

---
+++ headers/editor.h
@@ -0,0 +1,13 @@
+#ifndef EDITOR_H
+#define EDITOR_H
+#include <iostream>
+#include <gtk/gtk.h>
+
+using namespace std;
+
+const bool saveBAS();
+const bool saveExtension();
+
+const bool openProject();
+const bool openAbout();
+#endif // EDITOR_H
